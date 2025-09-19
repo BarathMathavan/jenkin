7da6f31a488338a996a66c2c2f7b25c91aff6b05
BarathMathavan <mathavanbarath6@gmail.com>
2025-09-19 12:05:26 +0530

    Add files via upload

---
+++ quiz_app.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class quiz_app{
+	public static void main(String[] args){
+		int score = 0;
+		String s1 = "what is the sum 8 + 9";
+		String o1 = "17";
+		String o2 = "18";
+		String o3 = "19";
+		String o4 = "17";
+		if(o1 == o4){
+			score++;
+		}
+		System.out.print("score:" + score);
+	}
+}
+		
+		
